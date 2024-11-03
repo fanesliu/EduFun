ad4eecfdb9f61293394b8138731d1c067f01a685
Fanes Liu <fns.liu1@gmail.com>
2024-11-03 20:56:57 +0700

    initial commit

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 @section('content')
+<img src="{{ asset('images/hero.png') }}" alt="hero" style="width: 100%; object-fit: cover;">
     <div class="container">
         @include('components.article_card')
     </div>
